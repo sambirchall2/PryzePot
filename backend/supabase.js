1d78dcf703cf89fdc91d2bdc37df56bd6af1bc5b
sambirchall2 <sambirch003@gmail.com>
2026-06-08 23:05:47 -0500

    Simplify Supabase client

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,14 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
-const ws = require("ws");
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_SECRET_KEY,
-    {
-        realtime: {
-            transport: ws
-        }
-    }
+    process.env.SUPABASE_SECRET_KEY
 );
 
 module.exports = supabase;
